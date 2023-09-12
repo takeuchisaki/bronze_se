732421a7ae1d09ed42c1b50f7a376b41f2bef39d
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-09-12 09:38:23 +0000

    Finish2_4

---
+++ Sample2_3.java
@@ -0,0 +1,10 @@
+class Sample2_3 {
+  public static void main(String[] args) {
+    int num1 = 10;
+    long num2 = 30;
+    // long num3 = 100000000000;
+    // float num4 = 10.0;
+    long num3 = 100000000000L;
+    float num4 = 10.0f;
+  }
+}
