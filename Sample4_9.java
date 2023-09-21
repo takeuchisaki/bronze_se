3ce11216e0fa749f99ff9e9ee28bf4fe0fe7bc41
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-09-21 08:43:58 +0000

    Finish4章

---
+++ Sample4_9.java
@@ -0,0 +1,13 @@
+class Sample4_9 {
+  public static void main(String[] args) {
+    for (int i = 0; ; i++) {
+      if (i == 3) {
+        break;
+      }
+      
+    System.out.println("i = " + i);
+    }
+    
+    System.out.println("for文の後処理");
+  }
+}
