fb83dd17fd861cece48246f1445db96e9044c0ed
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-09-30 09:47:59 +0000

    Finish6章

---
+++ Sample6/Sample6_6.java
@@ -0,0 +1,28 @@
+class Test {
+  int instanceVal = 100;
+  static int staticVal = 200;
+  void methodA() {
+    System.out.println("methodA():" + instanceVal);
+  }
+  static void methodB() {
+    System.out.println("methodB():" + staticVal);
+  }
+}
+
+class Sample6_6 {
+  public static void main(String[] args) {
+    // System.out.println(Test.staticVal);
+    System.out.println(Test.staticVal);
+    // Test.methodA();
+    Test.methodB();
+    System.out.println("----------------");
+    
+    Test t = new Test();
+    System.out.println(t.instanceVal);
+    System.out.println(t.staticVal);
+    
+    t.methodA();
+    t.methodB();
+    
+  }
+}
