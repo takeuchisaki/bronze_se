8decda361b84dbb96994c0a283111e858769189d
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-10-09 07:25:17 +0000

    Finish8_2

---
+++ Sample8/Sample8_1.java
@@ -0,0 +1,14 @@
+abstract class Super {
+  protected abstract void methodA();
+  public void methodB() {
+  }
+}
+
+class Sub extends Super {
+  protected void methodA() {
+  }
+  // public void methodA() {
+  // }
+  public void methodB() {
+  }
+}
---
+++ Sample8/Sample8_2.java
@@ -0,0 +1,17 @@
+abstract class X {
+  protected abstract void methodA();
+}
+
+abstract class Y extends X {
+}
+
+class Z extends Y {
+  protected void methodA() {
+  }
+  
+  // public void methodA() {
+  // }
+  
+  // void mehotdA() {
+  // }
+}
